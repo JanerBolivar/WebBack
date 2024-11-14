572d5ae904663159e8f2d9b318c3ee2886dcbc09
JanerBolivar <janerfabian83@gmail.com>
2024-11-14 12:31:04 -0500

    Variables de entorno

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,14 @@
 import dotenv from 'dotenv';
-
 dotenv.config();
 
 export const PORT = process.env.PORT || 3000;
 export const SECRET_KEY = process.env.SECRET_KEY;
+
+export const API_KEY = process.env.FB_APIKEY;
+export const AUTH_DOMAIN = process.env.FB_AUTHDOMAIN;
+export const PROJECT_ID = process.env.FB_PROJECTID;
+export const STORAGE_BUCKET = process.env.FB_STORAGEBUCKET;
+export const MESSAGING_SENDER_ID = process.env.FB_MESSAGINGSENDERID;
+export const APP_ID = process.env.FB_APPID;
+export const MEASUREMENT_ID = process.env.FB_MEASUREMENTID;
+export const DATABASE_URL = process.env.FB_DATABASEURL;
--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,21 +2,29 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 import { getStorage } from "firebase/storage";
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+  MEASUREMENT_ID,
+  DATABASE_URL
+} from "./config/index.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAIw0Qc75fTr7C0xYwMqlgafbxABEYImb0",
-  authDomain: "bitacora-peces.firebaseapp.com",
-  projectId: "bitacora-peces",
-  storageBucket: "bitacora-peces.appspot.com",
-  messagingSenderId: "479926767904",
-  appId: "1:479926767904:web:9b1b062bec0d1280e6c745",
-  measurementId: "G-B5BQHY5NPN",
-  databaseURL: "https://bitacora-peces-default-rtdb.firebaseio.com/"
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID,
+  databaseURL: DATABASE_URL
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
 const auth = getAuth(app);
 const database = getDatabase(app);
 const storage = getStorage(app);
